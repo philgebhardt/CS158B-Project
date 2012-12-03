10417a417692787bec6f41f7e41995f500450088
philgebhardt <gebhardt.phil@gmail.com>
2012-12-02 17:08:32 -0800

    Establishes random 'connections'.

--- NESimulator/Element.java
+++ NESimulator/Element.java
@@ -1,29 +1,22 @@
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.Random;
 
 public class Element extends Thread
-{
-    LinkedList<OrderedTree<String>> data;
-    
-    HashMap<String, String> tcpTable;
-    LinkedList<String> systemInfo;
+{    
+    OrderedTree<OID> tcpConnTable;
+    LinkedList<OID> systemInfo;
     
     static final Random random = new Random();
     
     public Element()
     {
-        this.data = new LinkedList<OrderedTree<String>>();
+        this.tcpConnTable = new OrderedTree<OID>(new OID("TCP_CONN_TABLE", null), new LinkedList<OrderedTree<OID>>());
+        this.systemInfo = new LinkedList<OID>();
     }
-    public Element(LinkedList<OrderedTree<String>> data)
+    public OrderedTree<OID> agentData()
     {
-        this.data = data;
-    }
-    public OrderedTree<String> agentData()
-    {
-        return new OrderedTree<String>("Root", data);
+
+        return tcpConnTable;
     }
 	@Override
 	public void run()
@@ -32,7 +25,7 @@ public class Element extends Thread
 		{
 			try
 			{
-				Thread.sleep(5000);
+				Thread.sleep(1000);
 			}
 			catch(InterruptedException e)
 			{
@@ -42,8 +35,36 @@ public class Element extends Thread
 		}
 		
 	}
-	static void doSomethingRandom()
+	void doSomethingRandom()
 	{
+	    int p;
+	    //Probabilistic Insertion
+	    p = random.nextInt(100);
+	    if(p < 50)
+	    {
+	        OID newOID = createRandomTCP_OID();
+	        tcpConnTable.listIterator().add(new OrderedTree<OID>(newOID, null));
+	    }
+	}
+	static OID createRandomTCP_OID()
+	{
+	    int q1,q2,q3,q4,p;
+	    String name = "";
 	    
+	    q1 = random.nextInt(255);
+	    q2 = random.nextInt(255);
+	    q3 = random.nextInt(255);
+	    q4 = random.nextInt(255);
+	    p = random.nextInt(1000);
+	    name += q1 + "." + q2 + "." + q3 + "." + q4 + "." + p + ".";
+	    
+	    q1 = random.nextInt(255);
+        q2 = random.nextInt(255);
+        q3 = random.nextInt(255);
+        q4 = random.nextInt(255);
+        p = random.nextInt(1000);
+        name += q1 + "." + q2 + "." + q3 + "." + q4 + "." + p;
+        
+        return new OID(name, "ESTABLISHED");
 	}
 }
