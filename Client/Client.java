06deed103c78818b76283e1235d183e1be2e9c5e
ctaslim <cameron_taslim@yahoo.com>
2012-12-05 14:58:09 -0800

    latest change``

--- Client/Client.java
+++ Client/Client.java
@@ -4,11 +4,24 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.*;
+import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
@@ -18,10 +31,14 @@ public class Client extends JFrame{
 	
 	char newline = '\n';
 	
-	JPanel west;
+	JPanel north1;
+	JPanel north2;
     JPanel north;
+	JPanel west;
     JPanel center;
     JPanel south;
+    JPanel east;
+    JPanel treeDisplay;
     
     JLabel agentAddress;
     JLabel rmonAddress;
@@ -29,20 +46,33 @@ public class Client extends JFrame{
     JLabel oid;
     JLabel textLog;
     JLabel mibTree;
-    
+    JLabel userLabel;
+	JLabel passwordLabel;
+	
+	JTextField userInput;
     JTextField oidInput;
     JTextField agentInput;
     JTextField rmonInput;
     JTextField commInput;
     
+    JPasswordField passwordInput;
+    
     JTextArea log;
-    JTextArea treeDisplay;
     
     JButton get; 
     JButton set; 
     JButton walk; 
     JButton trap; 
     JButton displayButton;
+    
+    String userString;
+    String passwordString;
+    String rmonString;
+    String agentString;
+    String communityString;
+    String oidString;
+    
+    ArrayList<TreeNode> list;
 	
 	public Client(String title)
 	{
@@ -51,11 +81,22 @@ public class Client extends JFrame{
 	}
 	public void setFrame()
 	{	
-	    west = new JPanel();
+		rmonString = "";
+	    agentString = "";
+	    communityString = "";
+	    oidString = "";
+		
+	    north1 = new JPanel();
+	    north2 = new JPanel();
 	    north = new JPanel();
+	    west = new JPanel();
 	    center = new JPanel();
 	    south = new JPanel();
+	    east = new JPanel();
 	    
+
+	    userLabel = new JLabel("User Name: ");
+	    passwordLabel = new JLabel("Password: ");
 	    agentAddress = new JLabel("Agent");
 	    rmonAddress = new JLabel("RMON");
 	    commString = new JLabel("Community String");
@@ -63,28 +104,79 @@ public class Client extends JFrame{
 	    textLog = new JLabel("LOG");
 	    mibTree = new JLabel("MIB Tree");
 	    
+
+	    userInput = new JTextField();
+	    userInput.setPreferredSize(new Dimension(145, 25));
+	    passwordInput = new JPasswordField();
+	    passwordInput.setPreferredSize(new Dimension(145, 25));
 	    oidInput = new JTextField();
-	    oidInput.setPreferredSize(new Dimension(300, 25));
+	    oidInput.setPreferredSize(new Dimension(145, 25));
 	    agentInput = new JTextField();
-	    agentInput.setPreferredSize(new Dimension(300, 25));
+	    agentInput.setPreferredSize(new Dimension(145, 25));
 	    rmonInput = new JTextField();
-	    rmonInput.setPreferredSize(new Dimension(300, 25));
+	    rmonInput.setPreferredSize(new Dimension(145, 25));
 	    commInput = new JTextField();
-	    commInput.setPreferredSize(new Dimension(300, 25));
+	    commInput.setPreferredSize(new Dimension(145, 25));
 	   
 	    log = new JTextArea();
-	    log.setPreferredSize(new Dimension(900, 550));
 	    log.setEditable(false);
-	    treeDisplay = new JTextArea();
-	    treeDisplay.setPreferredSize(new Dimension(200, 550));
-	    treeDisplay.setEditable(false);
+	    treeDisplay = new JPanel();
+	    treeDisplay.setPreferredSize(new Dimension(160, 555));
+	    treeDisplay.setLayout(new BoxLayout(treeDisplay, BoxLayout.Y_AXIS));
+	    
+	    list = new ArrayList<TreeNode>();
+	    list.add(new TreeNode("- tcpConnTable", "0"));
+	    list.add(new TreeNode("   - tcpConnEntry", "0.0"));
+	    list.add(new TreeNode("      - tcpConnState", "0.0.0"));
+	    list.add(new TreeNode("      - tcpConnLocalAddress", "0.0.1"));
+	    list.add(new TreeNode("      - tcpConnLocalPort", "0.0.2"));
+	    list.add(new TreeNode("      - tcpConnRemAddress", "0.0.3"));
+	    list.add(new TreeNode("      - tcpConnRemPort", "0.0.4"));
+	    for (int i = 0; i < list.size(); i++)
+	    {
+	    	final TreeNode nextNode = list.get(i);
+	    	nextNode.getLabel().addMouseListener(new MouseAdapter() {
+	            public void mouseClicked(MouseEvent e) {
+	                oidInput.setText(nextNode.getOID());
+	            }
+	        });
+	    	treeDisplay.add(nextNode.getLabel());
+	    }
+	    
+	    
 	    
 	    get = new JButton("GET");
 	    get.addActionListener(new ActionListener() {
  
             public void actionPerformed(ActionEvent e)
             {
+            	userString = userInput.getText();
+            	passwordString = passwordInput.getPassword().toString();
+            	try
+            	{
+            		MessageDigest m = MessageDigest.getInstance("MD5");
+            		m.reset();
+            		m.update(passwordString.getBytes(), 0, passwordString.length());
+            		byte messageDigest[] = m.digest();
+            		StringBuffer hexString = new StringBuffer();
+            		for(int i = 0; i < messageDigest.length;i++)
+            			hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            		updateLog(passwordString + ": " + hexString.toString());
+            	}
+            	catch(NoSuchAlgorithmException n)
+            	{
+            	}
+            	
+            	
+            	rmonString = rmonInput.getText();
+        	    agentString = agentInput.getText();
+        	    communityString = commInput.getText();
+        	    oidString = oidInput.getText();
                 updateLog("get " + oidInput.getText() + "...");
+                if (rmonString.isEmpty() == false && agentString.isEmpty())
+                	updateLog(communicate("get " + communityString + " " + oidString, rmonString));
+                else if (rmonString.isEmpty() && agentString.isEmpty() == false)
+                	updateLog(communicate("get " + communityString + " " + oidString, agentString));
                 done();
             }
         }); 
@@ -93,7 +185,16 @@ public class Client extends JFrame{
 	    	 
             public void actionPerformed(ActionEvent e)
             {
+            	rmonString = rmonInput.getText();
+        	    agentString = agentInput.getText();
+        	    communityString = commInput.getText();
+        	    oidString = oidInput.getText();
             	updateLog("set " + oidInput.getText() + "...");
+
+            	 if (rmonString.isEmpty() == false && agentString.isEmpty())
+            		 updateLog(communicate("set " + communityString + " " + oidString, rmonString));
+                 else if (rmonString.isEmpty() && agentString.isEmpty() == false)
+                   	 updateLog(communicate("set " + communityString + " " + oidString, agentString));
             	done();
             }
         }); 
@@ -102,7 +203,16 @@ public class Client extends JFrame{
 	    	 
             public void actionPerformed(ActionEvent e)
             {
+            	rmonString = rmonInput.getText();
+        	    agentString = agentInput.getText();
+        	    communityString = commInput.getText();
+        	    oidString = oidInput.getText();
             	updateLog("walk " + oidInput.getText() + "...");
+
+            	 if (rmonString.isEmpty() == false && agentString.isEmpty())
+            		 updateLog(communicate("walk " + communityString + " " + oidString, rmonString));
+                 else if (rmonString.isEmpty() && agentString.isEmpty() == false)
+                 	 updateLog(communicate("walk " + communityString + " " + oidString, agentString));
             	done();
             }
         }); 
@@ -111,48 +221,56 @@ public class Client extends JFrame{
 	    	 
             public void actionPerformed(ActionEvent e)
             { 
+            	rmonString = rmonInput.getText();
+        	    agentString = agentInput.getText();
+        	    communityString = commInput.getText();
+        	    oidString = oidInput.getText();
             	updateLog("trap " + oidInput.getText() + "...");
+
+            	 if (rmonString.isEmpty() == false && agentString.isEmpty())
+            		 updateLog(communicate("trap " + communityString + " " + oidString, rmonString));
+                 else if (rmonString.isEmpty() && agentString.isEmpty() == false)
+                 	 updateLog(communicate("trap " + communityString + " " + oidString, agentString));
             	done();
             }
         }); 
-	    displayButton = new JButton("Display Tree");
-	    displayButton.addActionListener(new ActionListener() {
-	    	 
-            public void actionPerformed(ActionEvent e)
-            {
-                updateLog("Retrieving MIB Tree");
-                updateLog("RMON address : " + rmonInput.getText());
-                updateLog("agent address : " + agentInput.getText());
-                updateLog("community string : " + commInput.getText());  
-                done();
-            }
-        }); 
-	    
-	    west.setLayout(new FlowLayout()); 
+
+	    west.setLayout(new BoxLayout(west, BoxLayout.Y_AXIS));
 	    west.setBackground(Color.GRAY);
-	    center.setLayout(new FlowLayout()); 
+	    center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
 	    center.setBackground(Color.GRAY);
-	    north.setLayout(new FlowLayout());
+		north2.setLayout(new FlowLayout());
+	    north2.setBackground(Color.GRAY);
+	    north1.setLayout(new FlowLayout());
+	    north1.setBackground(Color.GRAY);
+	    north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
 	    north.setBackground(Color.GRAY);
 	    south.setLayout(new FlowLayout());
 	    south.setBackground(Color.GRAY);
+	    east.setBackground(Color.GRAY);
+
+		north1.add(userLabel);
+		north1.add(userInput);
+		north1.add(passwordLabel);
+		north1.add(passwordInput);
+	    north2.add(rmonAddress);
+	    north2.add(rmonInput);
+	    north2.add(agentAddress);
+	    north2.add(agentInput);
+	    north2.add(commString);
+	    north2.add(commInput);
+	    north2.add(oid);
+	    north2.add(oidInput);
+	    north.add(north1);
+	    north.add(north2);
 	    
-	    north.add(rmonAddress);
-	    north.add(rmonInput);
-	    north.add(agentAddress);
-	    north.add(agentInput);
-	    north.add(commString);
-	    north.add(commInput);
-	    north.add(displayButton);
-	    
+	    west.setPreferredSize(new Dimension(160, 555));
 	    west.add(mibTree);
 	    west.add(treeDisplay);
 	    
 	    center.add(textLog);
 	    center.add(log);
 	    
-	    south.add(oid);
-	    south.add(oidInput);
 	    south.add(get);
 	    south.add(set);
 	    south.add(walk);
@@ -162,8 +280,9 @@ public class Client extends JFrame{
 	    add(west, BorderLayout.WEST);
 	    add(center, BorderLayout.CENTER);
 	    add(south, BorderLayout.SOUTH);
+	    add(east, BorderLayout.EAST);
 	    
-	    setSize(1240, 670);
+	    setSize(850, 700);
 		setVisible(true);
 		setResizable(false);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -178,5 +297,41 @@ public class Client extends JFrame{
 	{
 		updateLog("done!" + newline);
 		
+	}
+	private String communicate(String command, String ipString)
+	{
+		Socket echoSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String report = "Error";
+        try
+        {
+        	
+            echoSocket = new Socket(ipString, 4444);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+		    out.println(command);
+		    report = in.readLine();
+        }
+        catch (UnknownHostException e)
+        {
+            System.out.println("Don't know about host: " + echoSocket.getInetAddress());
+        }
+        catch (IOException e)
+        {
+            System.out.println("Couldn't get I/O for the connection to: " + ipString);
+            
+        }
+        try
+        {
+		    out.close();
+    		in.close();
+    		echoSocket.close();
+        }
+        catch (IOException e)
+        {
+        	System.out.println("Couldn't get I/O for the connection to: " + command);
+        }
+        return report;
 	}
 }
